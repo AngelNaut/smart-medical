f74b859f1f2fa29178af62167e83bb1caee669f4
Rosme-J227 <rosmejc.23456@gmail.com>
2026-04-16 23:57:42 -0400

    inyeccion de dependencias de la api

--- src/SmartMedical.API/Program.cs
+++ src/SmartMedical.API/Program.cs
@@ -1,20 +1,44 @@
 using Microsoft.EntityFrameworkCore;
 using SmartMedical.Infrastructure.Persistence;
+using SmartMedical.Application.Interfaces;
+using SmartMedical.Application.Services;
+using SmartMedical.Domain.Interfaces;
+using SmartMedical.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Configure Entity Framework Core with SQL Server
+// inyeccion de repositorios
+builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+builder.Services.AddScoped<IPatientRepository, PatientRepository>();
+
+// inyeccion de servicios
+builder.Services.AddScoped<IAppointmentService, AppointmentService>();
+builder.Services.AddScoped<IPatientService, PatientService>();
+builder.Services.AddScoped<PriorityService>();
+
+// Configuracion de CORS (Aceptar todo)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+// configuracion de entity framework core con sql server
 builder.Services.AddDbContext<SmartMedicalDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// configuracion del pipeline de solicitud http
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -28,7 +52,8 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-app.UseHttpsRedirection();
+ app.UseHttpsRedirection(); 
+app.UseCors("AllowAll"); // <-- Activa el CORS que configuramos arriba
 app.MapControllers();
 
 
